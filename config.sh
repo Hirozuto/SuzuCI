1c348eb3747047312d169afa44457a989e785a8c
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-09-21 18:46:45 +0800

    build for a20

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://github.com/TeamWin/android_device_samsung_a20 -b android-9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/a20 -b fox-9.0"
 
 export DEVICE="a20"
 export OEM="samsung"
@@ -23,6 +23,31 @@ DEPS=(
 
 # Extra Command
 export EXTRA_CMD="export OF_MAINTAINER=ItzKaguya"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/recovery"
+export FOX_REPLACE_BUSYBOX_PS="1"
+export FOX_USE_BASH_SHELL="1"
+export FOX_USE_LZMA_COMPRESSION="1"
+export FOX_USE_NANO_EDITOR="1"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+export LC_ALL="C"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_OTA_RES_DECRYPT="1"
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export OF_USE_NEW_MAGISKBOOT="1"
+export TARGET_ARCH="arm64"
+export TW_DEFAULT_LANGUAGE="en"
+export FOX_VERSION="R11.0"
+export FOX_R11="1"
+export USE_CCACHE="1"
+export OF_FLASHLIGHT_ENABLE="0"
+export OF_SCREEN_H="2340"
+export FOX_BUILD_TYPE="Beta"
+export FOX_ADVANCED_SECURITY="1"
+
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
