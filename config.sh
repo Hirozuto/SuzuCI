f226eba0cf63c89b8e366362143f5fd8f8446a46
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-10-01 07:29:56 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://github.com/MizuNotCool/recovery_device_samsung_a10s -b ofox"
+export DT_LINK="https://gitlab.com/OrangeFox/device/j7velte.git -b fox_9.0"
 
-export DEVICE="a10s"
+export DEVICE="j7velte"
 export OEM="samsung"
 
 # Build Target
@@ -25,31 +25,6 @@ DEPS=(
 export EXTRA_CMD="git clone https://github.com/MizuNotCool/avatar /root/avatar"
 export OF_MAINTAINER=ItzKaguya
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/bootdevice/by-name/recovery"
-export FOX_REPLACE_BUSYBOX_PS="1"
-export FOX_USE_BASH_SHELL="1"
-export FOX_USE_LZMA_COMPRESSION="1"
-export FOX_USE_NANO_EDITOR="1"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-export LC_ALL="C"
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
-export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
-export OF_OTA_RES_DECRYPT="1"
-export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
-export OF_USE_NEW_MAGISKBOOT="1"
-export TARGET_ARCH="arm64"
-export TW_DEFAULT_LANGUAGE="en"
-export FOX_VERSION="R11.0"
-export FOX_R11="1"
-export USE_CCACHE="1"
-export OF_FLASHLIGHT_ENABLE="1"
-export OF_SCREEN_H="1520"
-export FOX_BUILD_TYPE="ItzKaguya-Beta"
-export FOX_ADVANCED_SECURITY="1"
-export OF_FL_PATH1="/sys/devices/platform/flashlights_led191/led_flash"
-export OF_TARGET_DEVICES="a10s,a10sxx"
 export OF_MAINTAINER_AVATAR="/root/avatar/ItzKaguya.png"
 
 # Magisk
